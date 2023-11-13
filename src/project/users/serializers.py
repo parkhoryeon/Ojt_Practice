fe984e1f67a596d93a4d4cd7ca31b4ccc720019a
parkhoryeon <hrpark930908@naver.com>
2023-11-14 00:23:05 +0900

    Commit
    231114_0023

--- src/project/users/serializers.py
+++ src/project/users/serializers.py
@@ -2,16 +2,34 @@ from rest_framework import serializers
 from .models import User
 
 
-class UserSerializer(serializers.Serializer):
+# ################################################################################## 
+# Model 기반으로 자동으로 생성
+class UserSerializer(serializers.ModelSerializer):
 
-    pk = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=20)
+    class Meta:
+        model = User
+        fields = (
+            "__all__" # 해당 모델의 모든 field를 보여주겠다고 하는 것.
+            # "pk",
+            # "name",
+        )
+        # 특정 필드 제외
+        # exclude = (
+        #     "email",
+        # )
 
-    def create(self, validated_data):
-        print('VALIDATED_DATA : ', validated_data)
-        return User.objects.create(**validated_data)
+# ################################################################################## 
+# 직접 serializer 생성
+# class UserSerializer(serializers.Serializer):
 
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.save()
-        return instance
+#     pk = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField(max_length=20)
+
+#     def create(self, validated_data):
+#         print('VALIDATED_DATA : ', validated_data)
+#         return User.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get("name", instance.name)
+#         instance.save()
+#         return instance
--- src/project/users/urls.py
+++ src/project/users/urls.py
@@ -7,6 +7,21 @@ urlpatterns = [
     path('login/', views.custom_login, name='login'),
     path('logout/', views.custom_logout, name='logout'),
 
-    path('', views.users),
-    path('<int:pk>', views.user),
+    path('', views.UsersAPIViewSet.as_view(
+        {
+            "get": "list",
+            "post": "create",
+        }
+    )),
+    # path('', views.UsersAPI.as_view()),
+    # path('', views.users),
+    path('<int:pk>', views.UsersAPIViewSet.as_view(
+        {
+            "get": "retrieve",
+            "put": "partial_update",
+            "delete": "destroy",
+        }
+    )),
+    # path('<int:pk>', views.UserAPI.as_view()),
+    # path('<int:pk>', views.user),
 ]
